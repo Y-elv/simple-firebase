37a93a0665a447e29a84a8d592acd224860c069b
mugisha <mugishaelvis456@gmail.com>
2023-04-25 11:31:17 +0200

    chat message

---
+++ config.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+ 
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyAvms4oCb0MJUoIHopZUk2HznTP7E-FDEc",
+  //   authDomain: "cure-firebase.firebaseapp.com",
+  //   databaseURL: "https://cure-firebase-default-rtdb.firebaseio.com",
+  //   projectId: "cure-firebase",
+  //   storageBucket: "cure-firebase.appspot.com",
+  //   messagingSenderId: "728770205201",
+  //   appId: "1:728770205201:web:da95dfa0aa1661c8cd03b0",
+  //   measurementId: "G-TF9WPWXWET"
+  // };
+  
+  // // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
+  
---
+++ cure.js
@@ -0,0 +1,45 @@
+// let chat=document.getElementById("chat")
+// console.log(chat)
+// let btn=document.getElementById("btn")
+// console.log(btn)
+// let para=document.getElementById("content")
+// console.log(para)
+
+
+// import { getDatabase, ref,get, set, child, update, remove }
+//     from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"
+// db.collection("cure").get().then((data) => {
+//     console.log("all docs", data);
+    
+
+//     data.forEach((doc) => {
+//         // doc.data() is never undefined for query doc snapshots
+          
+//         console.log(doc.id, " => ", doc.data());
+        
+      
+   
+//         para.innerHTML=doc.data().name
+      
+//     });
+    
+// });
+
+// function insertData() {
+//     set(ref(db, "" + chat.value), {
+//         nameofstd: chat.value,
+        
+//     })
+//         .then(() => {
+//             alert("data store sucessfully");
+//         })
+//         .catch((error)=>{
+//             alert(" unsucessfully , error"+error);
+//         })
+
+// }
+// // assign event to btn and store them in database
+
+// btn.addEventListener("click",insertData)
+
+
